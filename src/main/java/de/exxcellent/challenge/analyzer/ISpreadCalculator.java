809f387306156fcb65dd38812d3931ca1cbe770f
Karsten Schulz <karsten.schulz@lennysoft.de>
2018-07-08 17:23:25 +0200

    analyzer: add java documentation to ISpreadCalculator
    add missing java doc to interface description

--- src/main/java/de/exxcellent/challenge/analyzer/ISpreadCalculator.java
+++ src/main/java/de/exxcellent/challenge/analyzer/ISpreadCalculator.java
@@ -1,6 +1,19 @@
 package de.exxcellent.challenge.analyzer;
 
+import lombok.NonNull;
+
+/**
+ * Interface to calculate the spread between two numeric values provided by the generic data type.
+ *
+ * @param <D> as generic type that provides the integer values.
+ */
 public interface ISpreadCalculator<D> {
 
-    int calculate(D data);
+    /**
+     * Calculate the spread between two numeric values, that are provided both from passed data object.
+     *
+     * @param data as generic type that should provide the numeric values to calculate the spread.
+     * @return the calculated integer value.
+     */
+    int calculate(@NonNull D data);
 }
