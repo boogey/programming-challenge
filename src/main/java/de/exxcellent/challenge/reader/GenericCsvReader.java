fdd055abccf8079f92dc05048b72e210898aa256
Karsten Schulz <karsten.schulz@lennysoft.de>
2018-07-08 18:48:47 +0200

    reader: request data source while requestData()

--- src/main/java/de/exxcellent/challenge/reader/GenericCsvReader.java
+++ src/main/java/de/exxcellent/challenge/reader/GenericCsvReader.java
@@ -1,10 +1,11 @@
 package de.exxcellent.challenge.reader;
 
 import com.fasterxml.jackson.databind.MapperFeature;
-import com.fasterxml.jackson.databind.ObjectReader;
 import com.fasterxml.jackson.dataformat.csv.CsvMapper;
 import com.fasterxml.jackson.dataformat.csv.CsvParser;
 import com.fasterxml.jackson.dataformat.csv.CsvSchema;
+import com.google.common.base.Preconditions;
+import com.google.errorprone.annotations.CanIgnoreReturnValue;
 import lombok.NonNull;
 
 import java.io.IOException;
@@ -13,18 +14,28 @@ import java.util.Collections;
 import java.util.Iterator;
 import java.util.Objects;
 
+/**
+ * Generic csv reader that use the jackson csv reader mechanism inside. Class implements the {@link IDataProvider} and
+ * Reader could be configured based on the {@link IDataSource} and the expected POJO class.
+ *
+ * @param <D> as generic type that contains the expected POJO class reference.
+ */
 public class GenericCsvReader<D> implements IDataProvider<D> {
-
 
     private Iterator<D> dataContent;
 
-    private ObjectReader reader;
+    private IDataSource source;
 
+    private Class<D> pojoClass;
+
+    /**
+     * Creates an new instance of {@link GenericCsvReader}.
+     */
     public GenericCsvReader() {
         this.dataContent = Collections.emptyIterator();
     }
 
-    public void configureReader(@NonNull final Reader reader, @NonNull final Class<D> pojoClass) {
+    private void configureReaderInternal(@NonNull final Reader reader, @NonNull final Class<D> pojoClass) {
         final CsvMapper mapper = new CsvMapper();
         mapper.enable(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES);
         mapper.enable(CsvParser.Feature.IGNORE_TRAILING_UNMAPPABLE);
@@ -36,13 +47,29 @@ public class GenericCsvReader<D> implements IDataProvider<D> {
                     .with(schema)
                     .readValues(reader);
         } catch (final IOException ioe) {
-            dataContent = null;
+            dataContent = Collections.emptyIterator();
         }
     }
 
+    @CanIgnoreReturnValue
     @Override
     public boolean requestData() {
+        configureReaderInternal(source.getSource(Reader.class), pojoClass);
         return Objects.nonNull(dataContent);
+    }
+
+    /**
+     * Configure the csv reader directly.
+     *
+     * @param source    as {@link IDataSource} to specify the source of the csv encoded structure.
+     * @param pojoClass as {@link Class} that represent the expected data class to directly create type-safe the
+     *                  object instances.
+     */
+    public void configureReader(@NonNull final IDataSource source, @NonNull final Class<D> pojoClass) {
+        Preconditions.checkArgument(source.provideSourceType(Reader.class), "data source not providing Reader");
+
+        this.source = source;
+        this.pojoClass = pojoClass;
     }
 
     @Override
--- src/test/java/de/exxcellent/challenge/reader/GenericCsvReaderTest.java
+++ src/test/java/de/exxcellent/challenge/reader/GenericCsvReaderTest.java
@@ -6,17 +6,16 @@ import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestFactory;
 
-import java.io.IOException;
-import java.io.Reader;
 import java.io.StringReader;
 import java.util.Arrays;
 import java.util.Collection;
 
-import static org.assertj.core.api.Assertions.assertThatNullPointerException;
-import static org.assertj.core.api.Assertions.fail;
+import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.api.SoftAssertions.assertSoftly;
 import static org.junit.jupiter.api.DynamicTest.dynamicTest;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class GenericCsvReaderTest {
 
@@ -29,55 +28,73 @@ class GenericCsvReaderTest {
                         .isThrownBy(() -> reader.configureReader(null, Object.class))
         ), dynamicTest("Class is null", () ->
                 assertThatNullPointerException()
-                        .isThrownBy(() -> reader.configureReader(mock(Reader.class), null))
+                        .isThrownBy(() -> reader.configureReader(mock(IDataSource.class), null))
         ));
     }
 
-    StringReader prepareWeatherData() {
-        return new StringReader(
-                "Day,MxT,MnT,AvT,AvDP,1HrP TPcpn,PDir,AvSp,Dir,MxS,SkyC,MxR,Mn,R AvSLP\n" +
-                        "1,88,59,74,53.8,0,280,9.6,270,17,1.6,93,23,1004.5\n" +
-                        "2,79,63,71,46.5,0,330,8.7,340,23,3.3,70,28,1004.5");
+    IDataSource prepareWeatherData() {
+        return new IDataSource() {
+            @Override
+            public boolean provideSourceType(final Class<?> sourceType) {
+                return true;
+            }
+
+            @Override
+            public <S> S getSource(final Class<S> expectedSource) {
+                return (S) new StringReader(
+                        "Day,MxT,MnT,AvT,AvDP,1HrP TPcpn,PDir,AvSp,Dir,MxS,SkyC,MxR,Mn,R AvSLP\n" +
+                                "1,88,59,74,53.8,0,280,9.6,270,17,1.6,93,23,1004.5\n" +
+                                "2,79,63,71,46.5,0,330,8.7,340,23,3.3,70,28,1004.5");
+            }
+        };
     }
 
     @Test
     @DisplayName("Read WeatherData")
     void readWeatherData() {
 
-        try (final Reader reader = prepareWeatherData()) {
-            final GenericCsvReader<WeatherData> csvReader = new GenericCsvReader<>();
-            csvReader.configureReader(reader, WeatherData.class);
-            assertSoftly(s -> {
-                s.assertThat(csvReader.requestData())
-                        .as("Request success")
-                        .isTrue();
-                s.assertThat(csvReader.hasNext())
-                        .as("Reader contains data")
-                        .isTrue();
-            });
-        } catch (final IOException ioe) {
-            fail("Unable to read from StringReader", ioe);
-        }
+        final GenericCsvReader<WeatherData> csvReader = new GenericCsvReader<>();
+        csvReader.configureReader(prepareWeatherData(), WeatherData.class);
+        assertSoftly(s -> {
+            s.assertThat(csvReader.requestData())
+                    .as("Request success")
+                    .isTrue();
+            s.assertThat(csvReader.hasNext())
+                    .as("Reader contains data")
+                    .isTrue();
+        });
     }
 
     @Test
     @DisplayName("Check CSV Parser")
     void checkWeatherData() {
+        final GenericCsvReader<WeatherData> csvReader = new GenericCsvReader<>();
+        csvReader.configureReader(prepareWeatherData(), WeatherData.class);
+        assertThat(csvReader.requestData())
+                .isTrue();
+        
+        final WeatherData first = csvReader.next();
+        assertSoftly(s -> {
+            s.assertThat(first.getDay())
+                    .isEqualTo("1");
+            s.assertThat(first.getMinTemp())
+                    .isEqualTo(59);
+            s.assertThat(first.getMaxTemp())
+                    .isEqualTo(88);
+        });
+    }
 
-        try (final Reader reader = prepareWeatherData()) {
-            final GenericCsvReader<WeatherData> csvReader = new GenericCsvReader<>();
-            csvReader.configureReader(reader, WeatherData.class);
-            final WeatherData first = csvReader.next();
-            assertSoftly(s -> {
-                s.assertThat(first.getDay())
-                        .isEqualTo("1");
-                s.assertThat(first.getMinTemp())
-                        .isEqualTo(59);
-                s.assertThat(first.getMaxTemp())
-                        .isEqualTo(88);
-            });
-        } catch (final IOException ioe) {
-            fail("Unable to read from StringReader", ioe);
-        }
+    @Test
+    @DisplayName("Unable to parse from source")
+    void unableToRead() {
+        final IDataSource source = mock(IDataSource.class);
+        when(source.provideSourceType(any())).thenReturn(true);
+        when(source.getSource(any())).thenThrow(RuntimeException.class);
+
+        final GenericCsvReader<WeatherData> csvReader = new GenericCsvReader<>();
+        csvReader.configureReader(source, WeatherData.class);
+
+        assertThatExceptionOfType(RuntimeException.class)
+                .isThrownBy(() -> csvReader.requestData());
     }
 }
